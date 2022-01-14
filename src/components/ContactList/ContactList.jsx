f3972b6c31f3ffc2888c1e907a97a4f6ebb2c33a
Annomaliiya <ivasyukanna@gmail.com>
2022-01-14 15:04:00 +0200

    refactoring with connected mockapi db

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { getVisibleContacts } from "../../redux/contacts/contacts-selectors";
+
+import { useDeleteContactMutation } from "../../redux/contacts/api";
+
+import PropTypes from "prop-types";
+import s from "./ContactList.module.css";
+
+function ContactList({ contacts }) {
+  const visibleContacts = useSelector((state) =>
+    getVisibleContacts(state, contacts)
+  );
+
+  const [deleteContact] = useDeleteContactMutation();
+
+  return (
+    <ul className={s.list}>
+      {visibleContacts.map(({ id, name, number }) => (
+        <li key={id}>
+          {name}: {number}
+          <button className={s.btnDelete} onClick={() => deleteContact(id)}>
+            X
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default ContactList;
+
+ContactList.propTypes = {
+  contacts: PropTypes.array,
+};
